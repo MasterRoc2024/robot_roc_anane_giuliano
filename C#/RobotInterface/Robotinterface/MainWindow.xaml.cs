970f21ef454c3be5dace1907218a25d93507000b
e105 <touloniut@gmail.com>
2024-03-19 12:09:45 +0100

    interface graphique

---
+++ C#/RobotInterface/Robotinterface/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.IO.Ports;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ExtendedSerialPort_NS;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Robotinterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ExtendedSerialPort serialPort1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            serialPort1 = new ExtendedSerialPort("COM8", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.Open();
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+        }
+
+        private void SerialPort1_DataReceived(object? sender, DataReceivedArgs e)
+        {
+            textBoxReception.Text += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+        }
+
+        private void True(object sender, RoutedEventArgs e)
+        {
+
+        }
+        bool toggle = true;
+        private void ButtonEnvoyer_Click_1(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+
+        }
+
+        private void SendMessage()
+        {
+            ///textBoxReception.Text += "recu: " + textBoxEmission.Text + "\n";
+            ///textBoxEmission.Text = "";
+            serialPort1.WriteLine("bonjour");
+        }
+
+        private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
+    }
+}
